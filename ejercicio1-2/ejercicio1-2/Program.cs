031e4ede55fac4579459c80913dba6cf6ded8411
victor paez <galevicp@gmail.com>
2023-03-13 18:49:41 -0500

    ejercicio 1-2 c#

---
+++ ejercicio1-2/ejercicio1-2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//ejercicio 1 escribir un programa que reciba nombre y lo imprima por pantalla
+Console.WriteLine("introduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola "+ nombre);
+
+//ejercicio dos escribir un ejercicio que reciba la hora y la muestre por pantalla
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
